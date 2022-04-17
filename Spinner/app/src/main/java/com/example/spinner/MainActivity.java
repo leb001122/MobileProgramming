9b734151f84869f6ed8a1db21248b354ded965b0
leb001122 <leb001122@icloud.com>
2022-04-17 23:48:25 +0900

    220417

---
+++ Spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO spinner는 selected, listview는 click
+
+    Spinner spinner;
+    TextView textResult;
+    ListView listView;
+    TextView textOutput;
+    String[] localDataset = {
+            "apple",
+            "orange",
+            "banana",
+            "strawberry",
+            "watermelon",
+            "kiwi",
+            "grape",
+            "a", "b", "c", "d", "e"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spn_choice);
+        textResult = (TextView) findViewById(R.id.text_result);
+        listView = (ListView) findViewById(R.id.listview);
+        textOutput = (TextView) findViewById(R.id.text_output);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                String spinner_item = adapterView.getItemAtPosition(pos).toString();
+                textResult.setText(spinner_item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                textOutput.setText("selected : " +localDataset[pos]);
+            }
+        });
+
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.labels_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, localDataset);
+        spinner.setAdapter(adapter);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, localDataset);
+        listView.setAdapter(arrayAdapter);
+    }
+}
